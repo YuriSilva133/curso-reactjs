32eb1be854bb109556af0cc07d4dfaab4c64d661
Yuri Silva <yuriteofilo38@gmail.com>
2024-07-27 18:36:05 -0300

    Pagina 404

--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import {BrowserRouter, Routes, Route, Router} from 'react-router-dom'
-
-import App from './pages/app';
-import Contato from './pages/contato'
+import Navegacao from './rotas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/contato' element={<Contato />} />
-      </Routes>
-    </BrowserRouter>
+      <Navegacao />
   </React.StrictMode>
 );
---
+++ src/pages/naoEncontrado/index.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+export default function NaoEncontrado() {
+    
+    return(
+        <div className='pagina-nao-encontrada'>
+            <h2>Pagina não encontrada!</h2>
+            <img className='icone-nao-encontrado' src="/assets/images/nao_encontrado.jpg" alt="" />
+        </div>
+    )
+}
