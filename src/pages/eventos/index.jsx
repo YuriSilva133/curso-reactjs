42268efbfb18839f2ad92886c81ba82f78587375
Yuri Silva <yuriteofilo38@gmail.com>
2024-07-30 10:30:59 -0300

    eventos

--- src/pages/eventos/index.jsx
+++ src/pages/eventos/index.jsx
@@ -1,6 +1,26 @@
 import './index.scss'
 
 export default function Eventos() {
+    function clicou() {
+        alert('O usuario clicou!')
+    }
+    
+    function moveu() {
+        alert('mouse passou!')
+    }
+
+    function alterou(e) {
+        //target = elemento que foi alterado "input"
+        //value = valor alterado
+        let novoValor = e.target.value //string
+        alert('valor alterado para: ' + novoValor)
+    }
+
+    function alterouCkeck(e) {
+        let novoValor = e.target.checked; //boolean
+        alert('[Chekbox/radio] alterado para: ' + novoValor)
+    }
+
     return (
         <div className='pagina-eventos pagina'>
             <header className='cabecalho'>
@@ -10,18 +30,21 @@ export default function Eventos() {
             <section className='secao'>
                 <h1>Entendendo eventos</h1>
 
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, dolores, in quasi sint alias nisi, architecto harum maxime eligendi pariatur quae minus quos quisquam? Iste veritatis error consectetur blanditiis reiciendis.</p>
+                <p onMouseMove={moveu}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, dolores, in quasi sint alias nisi, architecto harum maxime eligendi pariatur quae minus quos quisquam? Iste veritatis error consectetur blanditiis reiciendis.</p>
 
-                <input type="text" placeholder='Digite aqui alguma coisa' />
+                <input onChange={alterou} type="text" placeholder='Digite aqui alguma coisa' />
 
-                <select>
+                <textarea onChange={alterou} placeholder='Digite aqui'>
+                </textarea>
+
+                <select onChange={alterou}>
                     <option>Selecione</option>
                     <option>item A</option>
                     <option>item B</option>
                 </select>
 
                 <div className='grupo'>
-                    <input type="checkbox" name="" id="" /> Opção 1
+                    <input onChange={alterouCkeck} type="checkbox" name="" id="" /> Opção 1
                     <input type="checkbox" name="" id="" /> Opção 2
                 </div>
 
@@ -33,6 +56,9 @@ export default function Eventos() {
                         <input type="radio" name='gpo' /> Opção 2
                     </div>
                 </div>
+
+                <button onClick={clicou}>Clique aqui</button>
+
             </section>
         </div>
     )
